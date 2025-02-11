8d8a698e20dc1e34330d3792d30115f3dea7f945
Martin Šantavý <90237662+santavyM@users.noreply.github.com>
2025-02-11 21:29:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -98,3 +98,8 @@ def reset_game():
     board = chess.Board()
     print("♻️ Hra byla restartována!")
     return {"message": "Hra restartována!"}
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
